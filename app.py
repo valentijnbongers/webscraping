5970352a6d4aea2a8be4e7b53338b739d67c2c4d
valentijnbongers <valentijnbongers@icloud.com>
2024-06-17 14:34:02 +0800

    webscraping app

---
+++ app.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import pandas as pd
+import os
+URL = 'https://books.toscrape.com/catalogue/'
+
+books=[]
+Price=[]
+Stock_availability=[]
+Books_url=[]
+# For loop to crawl and get information from multiple pages.
+for page in range(1,2):
+    Source = requests.get('https://books.toscrape.com/catalogue/private-paris-private-10_958/index.html')
+    Scrape = BeautifulSoup(Source.text,'html.parser')
+    print(Scrape.prettify()) 
+
+    # Grabbing book titles
+    for article in Scrape.find_all('article'):
+        books.append(article.h3.a.text)
+        print(books)
+    #Grabbing book price
+        Price.append(article.find('div', class_ = 'product_price').p.text)
+        print(Price)
+    #Grabbing stock availability of each book
+        Stock = Scrape.find('p', class_ = 'instock availability').text.strip()
+        Stock_availability.append(Stock)
+        print(Stock_availability)
+        
+    #Grabbing the link of each book
+        for link in article.find_all('a',href = True):
+            url = link['href']
+        Books_url.append('https://books.toscrape.com/catalogue/' + url)
+        print(Books_url)   
+         
+Scraped_Data = {'TITLE':books,'PRICE':Price,'STOCK AVAILABILTY':Stock_availability,'URL':Books_url}
+Scraped_Books = pd.DataFrame(Scraped_Data)
+Scraped_Books[:5]
+Scraped_Books.to_csv('SCB.csv',index = None)
